f26e31510761bcc7ca065c8ba4048e2a4a37eeb7
boochow <boochowp@gmail.com>
2023-05-23 17:55:35 +0900

    change path to eurorack dir, remove maximilian because it is not used

--- braids/config.mk
+++ braids/config.mk
@@ -9,14 +9,15 @@ PROJECT_TYPE := synth
 # Sources
 #
 
-BRAIDSDIR = ../eurorack/braids
-STMLIBDIR = ../eurorack/stmlib
+EURORACKDIR = ../eurorack
+BRAIDSDIR   = $(EURORACKDIR)/braids
+STMLIBDIR   = $(EURORACKDIR)/stmlib
 
 # C sources
 CSRC = header.c
 
 # C++ sources
-CXXSRC = unit.cc ../Maximilian/src/maximilian.cpp $(BRAIDSDIR)/digital_oscillator.cc $(BRAIDSDIR)/resources.cc $(BRAIDSDIR)/quantizer.cc $(BRAIDSDIR)/macro_oscillator.cc $(BRAIDSDIR)/analog_oscillator.cc $(STMLIBDIR)/utils/random.cc
+CXXSRC = unit.cc $(BRAIDSDIR)/digital_oscillator.cc $(BRAIDSDIR)/resources.cc $(BRAIDSDIR)/quantizer.cc $(BRAIDSDIR)/macro_oscillator.cc $(BRAIDSDIR)/analog_oscillator.cc $(STMLIBDIR)/utils/random.cc
 
 # List ASM source files here
 ASMSRC = 
@@ -27,7 +28,7 @@ ASMXSRC =
 # Include Paths
 #
 
-UINCDIR  = ../Maximilian/src $(STMLIBDIR) $(STMLIBDIR)/third_party/STM $(STMLIBDIR)/third_party/STM/STM32F10x_StdPeriph_Driver/inc/ $(STMLIBDIR)/third_party/STM/CMSIS/CM3_f10x
+UINCDIR  = $(EURORACKDIR) $(STMLIBDIR) $(STMLIBDIR)/third_party/STM $(STMLIBDIR)/third_party/STM/STM32F10x_StdPeriph_Driver/inc/ $(STMLIBDIR)/third_party/STM/CMSIS/CM3_f10x
 
 ##############################################################################
 # Library Paths
