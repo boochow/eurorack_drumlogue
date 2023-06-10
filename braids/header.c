2086e9d46400bcd5169b171aea824a5a15584cee
boochow <boochowp@gmail.com>
2023-06-10 14:22:31 +0900

    change the display name of curve of EG

--- braids/header.c
+++ braids/header.c
@@ -18,8 +18,8 @@ const __unit_header unit_header_t unit_header = {
     .unit_id = 0x02010000U, // Product number(02),Unit type(01=Synth),reserved
     .version = 0x00010000U, // major.minor.patch (major<<16 minor<<8 patch).
     .name = "Lillian",
-    .num_presets = 3,       // Number of internal presets this unit has
-    .num_params = 24,        // Number of parameters for this unit, max 24
+    .num_presets = 6,       // Number of internal presets this unit has
+    .num_params = 24,       // Number of parameters for this unit, max 24
     .params = {
         // Format: min, max, center, default, type, fractional, frac. type, <reserved>, name
         // See common/runtime.h for type enum and unit_param_t structure
@@ -31,7 +31,7 @@ const __unit_header unit_header_t unit_header = {
         {-256, 255, 0, 0, k_unit_param_type_none, 0, 0, 0, {"Color"}},
 
         // Page 2
-        {0, 127, 0, 127, k_unit_param_type_none, 0, 0, 0, {"A:EX/LN "}},
+        {0, 127, 0, 127, k_unit_param_type_none, 0, 0, 0, {"A:Curve"}},
         {0, 6, 0, 0, k_unit_param_type_strings, 0, 0, 0, {"Trigger"}},
         {0, 127, 0, 0, k_unit_param_type_none, 0, 0, 0, {"Attack"}},
         {0, 127, 0, 48, k_unit_param_type_none, 0, 0, 0, {"Decay"}},
@@ -43,7 +43,7 @@ const __unit_header unit_header_t unit_header = {
         {0, 15, 0, 0, k_unit_param_type_none, 0, 0, 0, {"FM Int"}},
 
         // Page 4
-        {0, 127, 0, 0, k_unit_param_type_none, 0, 0, 0, {"B:EX/LN"}},
+        {0, 127, 0, 0, k_unit_param_type_none, 0, 0, 0, {"B:Curve"}},
         {0, 6, 0, 0, k_unit_param_type_strings, 0, 0, 0, {"Trigger"}},
         {0, 127, 0, 0, k_unit_param_type_none, 0, 0, 0, {"Attack"}},
         {0, 127, 0, 48, k_unit_param_type_none, 0, 0, 0, {"Decay"}},
